eafd3f6476f1b66ab00fad30ba9009df8078e076
Ricardo Plansky <ricardo.plansky@yahoo.com>
2015-08-03 12:13:35 -0300

    Created generic exception to group domain exception

--- src/Exception/RequestException.php
+++ src/Exception/RequestException.php
@@ -1,11 +1,9 @@
 <?php
 namespace LeroyMerlin\ExactTarget\Exception;
 
-use RuntimeException;
-
 /**
 * Exception to throw when a request call fails
 */
-class RequestException extends RuntimeException
+class RequestException extends ExactTargetClientException
 {
 }
