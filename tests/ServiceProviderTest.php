45b3816c4b7b441dc5505288ae26f3196f462c7b
Ravan Scafi <rscafi@gmail.com>
2015-08-28 11:04:32 -0300

    Updated tests to cover everything.

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -1,32 +1,53 @@
 <?php
 namespace LeroyMerlin\ExactTarget;
 
+use Illuminate\Foundation\Application;
 use Mockery as m;
 use PHPUnit_Framework_TestCase;
 
 class ServiceProviderTest extends PHPUnit_Framework_TestCase
 {
-    public function setUp()
-    {
-        $this->markTestSkipped('ServiceProvider has changed and the tests should be reviewed.');
-    }
-
     public function testShouldHandleConfigsOnBoot()
     {
         // Set
-        $app      = m::mock('Illuminate\Foundation\Application');
-        $provider = m::mock(ServiceProvider::class . '[publishes,mergeConfigFrom]', [$app]);
+        $app      = m::mock(Application::class);
+        $provider = m::mock(ServiceProvider::class . '[handleConfig, publishes]', [$app]);
 
         // Expectation
+        $provider->shouldReceive('handleConfig')
+            ->once();
+
         $provider->shouldReceive('publishes')
             ->once();
 
-        $provider->shouldReceive('mergeConfigFrom')
-            ->once()
-            ->with(m::any(), 'laravel-exacttarget');
-
         // Assertion
         $provider->boot();
+    }
+
+    public function testShouldRegister()
+    {
+        // Set
+        $app      = m::mock(Application::class);
+        $provider = new ServiceProvider($app);
+
+        // Expectation
+        $app->shouldReceive('bind')
+            ->once()
+            ->with(Client::class, [$provider, 'registerClient']);
+
+        // Assertion
+        $provider->register();
+    }
+
+    public function testShouldRegisterClient()
+    {
+        // Set
+        $provider = new ServiceProvider(m::mock(Application::class));
+
+        $this->assertInstanceOf(
+            Client::class,
+            $provider->registerClient()
+        );
     }
 }
 
@@ -34,5 +55,11 @@ if (! function_exists('config_path')) {
     function config_path($file)
     {
         return 'foo/' . $file;
+    }
+}
+if (! function_exists('config')) {
+    function config($key)
+    {
+        return 'value:' . $key;
     }
 }
